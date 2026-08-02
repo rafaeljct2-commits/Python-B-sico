36814c1ed220062a4528a54220d58eaa87f0ce67
rafaeljct2-commits <rafaeljct2@gmail.com>
2026-08-02 20:34:31 -0300

    Update pong.py

--- pygame/pong.py
+++ pygame/pong.py
@@ -9,7 +9,13 @@ tela = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption("Pong")
 
 barra_y = 250
-velocidade = 5
+velocidade_barra = 5
+
+bola_x = 400
+bola_y = 300
+
+velocidade_bola_x = 4
+velocidade_bola_y = 4
 
 rodando = True
 
@@ -21,14 +27,21 @@ while rodando:
     teclas = pygame.key.get_pressed()
 
     if teclas[pygame.K_UP]:
-        barra_y -= velocidade
+        barra_y -= velocidade_barra
 
     if teclas[pygame.K_DOWN]:
-        barra_y += velocidade
+        barra_y += velocidade_barra
+
+    bola_x += velocidade_bola_x
+    bola_y += velocidade_bola_y
+
+    if bola_y <= 0 or bola_y >= altura:
+        velocidade_bola_y *= -1
 
     tela.fill((0, 0, 0))
 
     pygame.draw.rect(tela, (255, 255, 255), (50, barra_y, 20, 100))
+    pygame.draw.circle(tela, (255, 255, 255), (bola_x, bola_y), 10)
 
     pygame.display.flip()
 
