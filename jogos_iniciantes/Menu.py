876ec907941aa25fffdf7764aed2b71a63c1249a
rafaeljct2-commits <rafaeljct2@gmail.com>
2026-08-02 20:20:07 -0300

    Create Menu.py

---
+++ jogos_iniciantes/Menu.py
@@ -0,0 +1,15 @@
+print("=== MENU ===")
+print("1 - Calculadora")
+print("2 - Tabuada")
+print("3 - Sair")
+
+opcao = input("Escolha uma opção: ")
+
+if opcao == "1":
+    print("Abrindo calculadora...")
+elif opcao == "2":
+    print("Abrindo tabuada...")
+elif opcao == "3":
+    print("Saindo...")
+else:
+    print("Opção inválida!")
