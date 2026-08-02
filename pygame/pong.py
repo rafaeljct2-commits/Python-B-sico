543efcd6a9854b46d119e9a82467030f73a44d8f
rafaeljct2-commits <rafaeljct2@gmail.com>
2026-08-02 20:33:08 -0300

    Update pong.py

--- pygame/pong.py
+++ pygame/pong.py
@@ -2,8 +2,14 @@ import pygame
 
 pygame.init()
 
-tela = pygame.display.set_mode((800, 600))
+largura = 800
+altura = 600
+
+tela = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption("Pong")
+
+barra_y = 250
+velocidade = 5
 
 rodando = True
 
@@ -12,7 +18,18 @@ while rodando:
         if evento.type == pygame.QUIT:
             rodando = False
 
+    teclas = pygame.key.get_pressed()
+
+    if teclas[pygame.K_UP]:
+        barra_y -= velocidade
+
+    if teclas[pygame.K_DOWN]:
+        barra_y += velocidade
+
     tela.fill((0, 0, 0))
+
+    pygame.draw.rect(tela, (255, 255, 255), (50, barra_y, 20, 100))
+
     pygame.display.flip()
 
 pygame.quit()
