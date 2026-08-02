f1837d67d0316ef53f104d70fd705001502c55a1
rafaeljct2-commits <rafaeljct2@gmail.com>
2026-08-02 19:33:57 -0300

    Create jogo_da_velha.py

---
+++ jogos_iniciantes/jogo_da_velha.py
@@ -0,0 +1,9 @@
+tabuleiro = [" ", " ", " ",
+             " ", " ", " ",
+             " ", " ", " "]
+
+print(tabuleiro[0], "|", tabuleiro[1], "|", tabuleiro[2])
+print("---------")
+print(tabuleiro[3], "|", tabuleiro[4], "|", tabuleiro[5])
+print("---------")
+print(tabuleiro[6], "|", tabuleiro[7], "|", tabuleiro[8])
