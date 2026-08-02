b176a1ea36a29e0782e611934e3cc7fdf9d468fc
rafaeljct2-commits <rafaeljct2@gmail.com>
2026-08-02 20:04:52 -0300

    Update pedra_papel_tesoura.py

--- jogos_iniciantes/pedra_papel_tesoura.py
+++ jogos_iniciantes/pedra_papel_tesoura.py
@@ -2,18 +2,24 @@ import random
 
 opcoes = ["pedra", "papel", "tesoura"]
 
-jogador = input("Escolha pedra, papel ou tesoura: ").lower()
-computador = random.choice(opcoes)
+while True:
+    jogador = input("Escolha pedra, papel ou tesoura: ").lower()
+    computador = random.choice(opcoes)
 
-print("Computador escolheu:", computador)
+    print("Computador escolheu:", computador)
 
-if jogador == computador:
-    print("Empate!")
-elif (
-    (jogador == "pedra" and computador == "tesoura") or
-    (jogador == "papel" and computador == "pedra") or
-    (jogador == "tesoura" and computador == "papel")
-):
-    print("Você ganhou!")
-else:
-    print("Você perdeu!")
+    if jogador == computador:
+        print("Empate!")
+    elif (
+        (jogador == "pedra" and computador == "tesoura")
+        or (jogador == "papel" and computador == "pedra")
+        or (jogador == "tesoura" and computador == "papel")
+    ):
+        print("Você ganhou!")
+    else:
+        print("Você perdeu!")
+
+    continuar = input("Jogar novamente? (s/n): ").lower()
+
+    if continuar != "s":
+        break
