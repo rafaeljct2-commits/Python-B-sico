55d057c4656f5c3daf145bd80e64fe16789bf8b3
rafaeljct2-commits <rafaeljct2@gmail.com>
2026-08-02 20:01:54 -0300

    Update adivinhacao.py

--- jogos_iniciantes/adivinhacao.py
+++ jogos_iniciantes/adivinhacao.py
@@ -4,10 +4,11 @@ numero_secreto = random.randint(1, 10)
 
 print("=== Jogo da Adivinhação ===")
 
-palpite = int(input("Digite um número de 1 a 10: "))
+while True:
+    palpite = int(input("Digite um número de 1 a 10: "))
 
-if palpite == numero_secreto:
-    print("Parabéns! Você acertou!")
-else:
-    print("Você errou!")
-    print("O número era:", numero_secreto)
+    if palpite == numero_secreto:
+        print("Parabéns! Você acertou!")
+        break
+    else:
+        print("Tente novamente!")
