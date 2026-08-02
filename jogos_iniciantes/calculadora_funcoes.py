c5a105d961972ef91ec3dc00568492fbc5d4cd06
rafaeljct2-commits <rafaeljct2@gmail.com>
2026-08-02 20:18:45 -0300

    Create calculadora_funcoes.py

---
+++ jogos_iniciantes/calculadora_funcoes.py
@@ -0,0 +1,11 @@
+def somar(a, b):
+    return a + b
+
+def subtrair(a, b):
+    return a - b
+
+num1 = float(input("Primeiro número: "))
+num2 = float(input("Segundo número: "))
+
+print("Soma:", somar(num1, num2))
+print("Subtração:", subtrair(num1, num2))
