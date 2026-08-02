b2efc588c4754707bcfc2183b083d1c94589ecb1
rafaeljct2-commits <rafaeljct2@gmail.com>
2026-08-02 20:08:31 -0300

    Update cobrinha.py

--- pygame/cobrinha.py
+++ pygame/cobrinha.py
@@ -1,4 +1,4 @@
-import pygame
+  import pygame
 
 pygame.init()
 
@@ -6,7 +6,12 @@ largura = 800
 altura = 600
 
 tela = pygame.display.set_mode((largura, altura))
-pygame.display.set_caption("Jogo da Cobrinha")
+pygame.display.set_caption("Cobrinha")
+
+x = 400
+y = 300
+
+velocidade = 5
 
 rodando = True
 
@@ -15,7 +20,24 @@ while rodando:
         if evento.type == pygame.QUIT:
             rodando = False
 
+    teclas = pygame.key.get_pressed()
+
+    if teclas[pygame.K_LEFT]:
+        x -= velocidade
+
+    if teclas[pygame.K_RIGHT]:
+        x += velocidade
+
+    if teclas[pygame.K_UP]:
+        y -= velocidade
+
+    if teclas[pygame.K_DOWN]:
+        y += velocidade
+
     tela.fill((0, 0, 0))
+
+    pygame.draw.rect(tela, (0, 255, 0), (x, y, 20, 20))
+
     pygame.display.flip()
 
-pygame.quit()
+pygame.quit()  
