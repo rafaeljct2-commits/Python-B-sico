b703688c4d24cd477d0038bde876c92d86a6b0ca
rafaeljct2-commits <rafaeljct2@gmail.com>
2026-08-02 20:13:56 -0300

    Create calculadora.py

---
+++ jogos_iniciantes/calculadora.py
@@ -0,0 +1,7 @@
+num1 = float(input("Primeiro número: "))
+num2 = float(input("Segundo número: "))
+
+print("Soma:", num1 + num2)
+print("Subtração:", num1 - num2)
+print("Multiplicação:", num1 * num2)
+print("Divisão:", num1 / num2)
