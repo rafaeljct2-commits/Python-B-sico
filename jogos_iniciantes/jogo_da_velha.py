637dd6a69f2739451b06e22387da91115bd298cc
rafaeljct2-commits <rafaeljct2@gmail.com>
2026-08-02 20:05:56 -0300

    Update jogo_da_velha.py

--- jogos_iniciantes/jogo_da_velha.py
+++ jogos_iniciantes/jogo_da_velha.py
@@ -1,9 +1,19 @@
-tabuleiro = [" ", " ", " ",
-             " ", " ", " ",
-             " ", " ", " "]
+tabuleiro = [
+    " ", " ", " ",
+    " ", " ", " ",
+    " ", " ", " "
+]
 
-print(tabuleiro[0], "|", tabuleiro[1], "|", tabuleiro[2])
-print("---------")
-print(tabuleiro[3], "|", tabuleiro[4], "|", tabuleiro[5])
-print("---------")
-print(tabuleiro[6], "|", tabuleiro[7], "|", tabuleiro[8])
+while True:
+    print(tabuleiro[0], "|", tabuleiro[1], "|", tabuleiro[2])
+    print("---------")
+    print(tabuleiro[3], "|", tabuleiro[4], "|", tabuleiro[5])
+    print("---------")
+    print(tabuleiro[6], "|", tabuleiro[7], "|", tabuleiro[8])
+
+    posicao = int(input("Escolha uma posição (0 a 8): "))
+
+    if tabuleiro[posicao] == " ":
+        tabuleiro[posicao] = "X"
+    else:
+        print("Posição ocupada!")
